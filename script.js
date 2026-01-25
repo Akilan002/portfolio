0527de39091bcedfc4b462dedb666651cf086010
Akilan Mohan <akilanakil1020@gmail.com>
2026-01-25 23:39:43 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+
+////////////// slider
+document.addEventListener("DOMContentLoaded", () => {
+
+  let current = 0;
+  const slides = document.querySelectorAll(".slide");
+
+  function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[index].classList.add("active");
+  }
+
+  window.nextSlide = function () {
+    current = (current + 1) % slides.length;
+    showSlide(current);
+  };
+
+  window.prevSlide = function () {
+    current = (current - 1 + slides.length) % slides.length;
+    showSlide(current);
+  };
+
+});
+
+////////// header border
+
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+
+  if (window.scrollY > 30) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
+//////////// menu
+
+function openMenu() {
+  document.getElementById("navMenu").classList.add("active");
+}
+
+function closeMenu() {
+  document.getElementById("navMenu").classList.remove("active");
+}
+
